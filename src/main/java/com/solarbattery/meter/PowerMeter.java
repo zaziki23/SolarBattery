4537567c1b7205b3ecf7ce4101a01740cdf2c0bf
eschimanek <handshake987@web.de>
2019-11-17 12:51:53 +0100

    changes

---
+++ src/main/java/com/solarbattery/meter/PowerMeter.java
@@ -0,0 +1,35 @@
+package com.solarbattery.meter;
+
+import ws.palladian.retrieval.DocumentRetriever;
+import ws.palladian.retrieval.parser.json.JsonArray;
+import ws.palladian.retrieval.parser.json.JsonObject;
+
+import java.util.Optional;
+
+public class PowerMeter {
+    private double power = 0.0;
+    private double totalPower = 0.0;
+    private String url = null;
+
+    public PowerMeter(String url) {
+        this.url = url;
+    }
+
+
+    public double getPower() {
+        Double power = null;
+
+        try {
+            JsonObject jsonObject = new DocumentRetriever().tryGetJsonObject(url + "/status");
+            JsonArray meters = jsonObject.tryGetJsonArray("meters");
+            JsonObject firstMeter = meters.tryGetJsonObject(0);
+            power = firstMeter.tryGetDouble("power");
+            Double total = firstMeter.tryGetDouble("total");
+            this.power = power;
+            this.totalPower = total;
+        } catch ( Exception e) {
+            e.printStackTrace();
+        }
+        return Optional.ofNullable(power).orElse(0.0);
+    }
+}
